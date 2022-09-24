511e8c1206d536cdf3b16fe382321f2a1fff0fdc
Saiksha Desai <sd0040@srmist.edu.in>
2022-09-24 13:08:32 +0530

    Create fit.cpp

---
+++ fit.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t; 
+    cin>>t;
+    for(int i=0; i<t; i++){
+        int x;
+        cin>>x;
+        cout<<x*10<<endl;
+    }
+	// your code goes here
+	return 0;
+}
