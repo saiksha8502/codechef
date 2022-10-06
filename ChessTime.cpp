2965ddcbb6b9aef5599eec066652c06b1c4b2221
Saiksha Desai <sd0040@srmist.edu.in>
2022-10-06 10:26:52 +0530

    Create ChessTime.cpp

---
+++ ChessTime.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x;
+        cin>>x;
+        
+        cout<<(x*60)/20<<endl;    }
+	// your code goes here
+	return 0;
+}
